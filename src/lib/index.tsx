7714637968859c4467e00bcfabe2d8b469a9c6a4
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-10-15 17:17:39 +0530

    working?

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -21,6 +21,7 @@ import '@codingame/monaco-vscode-json-language-features-default-extension'
 import '@codingame/monaco-vscode-typescript-language-features-default-extension'
 import '@codingame/monaco-vscode-html-language-features-default-extension'
 import '@codingame/monaco-vscode-css-language-features-default-extension'
+// import '@codingame/monaco-vscode-emmet-default-extension'
 import './index.css'
 
 import * as monaco from 'monaco-editor'
